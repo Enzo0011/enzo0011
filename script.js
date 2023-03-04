d6bcdc17e65e48a8a4a28bbce020d9abe116440a
Enzo0011 <enzogdev@gmail.com>
2023-03-04 23:41:21 +0100

    change

--- script.js
+++ script.js
@@ -105,34 +105,6 @@ function startAnimation() {
 			offset: "-=875",
 			delay: (el, i, l) => 80 * (l - i),
 		});
-	anime
-		.timeline({ loop: false })
-		.add({
-			targets: ".ml11 .line",
-			scaleY: [0, 1],
-			opacity: [0.5, 1],
-			easing: "easeOutExpo",
-			duration: 700,
-		})
-		.add({
-			targets: ".ml11 .line",
-			translateX: [
-				0,
-				document.querySelector(".ml11 .letters").getBoundingClientRect()
-					.width + 10,
-			],
-			easing: "easeOutExpo",
-			duration: 700,
-			delay: 100,
-		})
-		.add({
-			targets: ".ml11 .letter",
-			opacity: [0, 1],
-			easing: "easeOutExpo",
-			duration: 600,
-			offset: "-=775",
-			delay: (el, i) => 34 * (i + 1),
-		});
 	anime.timeline({ loop: false }).add({
 		targets: ".ml13 .letter",
 		translateY: [100, 0],
