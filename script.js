e2540d50c017e1fc8d2e8d29cca166a58030f4f6
Enzo0011 <enzogdev@gmail.com>
2023-03-07 16:45:53 +0100

    add animation

--- script.js
+++ script.js
@@ -147,6 +147,14 @@ function startAnimation() {
 		duration: 2250,
 		delay: (el, i) => 150 * (i + 1),
 	});
+	anime.timeline({ loop: false }).add({
+		targets: ".ml15 .word",
+		scale: [14, 1],
+		opacity: [0, 1],
+		easing: "easeOutCirc",
+		duration: 800,
+		delay: (el, i) => 800 * i,
+	});
 }
 
 var ticking = false,
